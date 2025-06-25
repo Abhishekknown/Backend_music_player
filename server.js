96055038c3d1e18196d0bf7341e88de6a8745328
Abhishek Manjhi <90549183+Abhishekknown@users.noreply.github.com>
2025-06-25 20:11:22 +0530

    remove node_module

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ app.use('/api/songs', songRoute);
 
 // MongoDB Connections
 
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
+mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log('Connected to MongoDB');
 
     // Start Server only after DB connection
